0622c072dde75fd8c8280e8126ccc8edb49be2ec
Reggie Qin <reggieqin@microsoft.com>
2021-07-29 14:41:20 +0800

    Use keyvalut to manage id/pwd

--- MyTimerTest/Function1.cs
+++ MyTimerTest/Function1.cs
@@ -6,28 +6,38 @@ using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+using System.Linq;
+using Microsoft.Azure.ServiceBus;
+using System.Text;
+using System.Collections.Generic;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Identity;
+
 
 namespace MyTimerTest
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async Task Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"Reggie Timer trigger function executed at: {DateTime.Now}");
             //log.LogInformation($"Reggie Config: {Environment.GetEnvironmentVariable("Database:DataSource")}");
             //log.LogInformation($"Reggie test: {Environment.GetEnvironmentVariable("version")}");
 
-            ServiceBusClient client = new ServiceBusClient(Environment.GetEnvironmentVariable("SBConnectString"));
-            ServiceBusSender sender = client.CreateSender(Environment.GetEnvironmentVariable("QName"));
+            //ServiceBusClient client = new ServiceBusClient(Environment.GetEnvironmentVariable("SBConnectString"));
+            //ServiceBusSender sender = client.CreateSender(Environment.GetEnvironmentVariable("QName"));
+            //ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
 
-            ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+            SecretClient secretClient = new SecretClient(new Uri(Environment.GetEnvironmentVariable("KeyVaultEndpoint")), new DefaultAzureCredential());
+            KeyVaultSecret userId = await secretClient.GetSecretAsync($"SQL-userId");
+            KeyVaultSecret password = await secretClient.GetSecretAsync($"SQL-password");
 
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-
             builder.DataSource = Environment.GetEnvironmentVariable("DataSource");
-            builder.UserID = Environment.GetEnvironmentVariable("UserID");
-            builder.Password = Environment.GetEnvironmentVariable("Password");
+            builder.UserID = userId.Value;
+            builder.Password = password.Value;
             builder.InitialCatalog = Environment.GetEnvironmentVariable("SchedulerDB");
 
             log.LogInformation($"DB configuration source:{builder.DataSource} userId:{builder.UserID} pwd:{builder.Password} db:{builder.InitialCatalog}");
@@ -35,43 +45,71 @@ namespace MyTimerTest
             SqlConnection sqlConnection = new SqlConnection(builder.ConnectionString);
             sqlConnection.Open();
 
-            string sql = @"select Id, BotId, UserId, userName, ChannleId, Locale, ServiceUrl from Conversations " +
-                "where ChannleId = 'msteams'";
-
-            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            // Get unique appId
+            List<string> appList = new List<string>();
+            string uniqueAppidSql = @"select distinct AppId from userinfo";
+            using (SqlCommand command = new SqlCommand(uniqueAppidSql, sqlConnection))
             {
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        var id = reader.GetString(0);
-                        var botId = reader.GetString(1);
-                        var userId = reader.GetString(2);
-                        var userName = reader.GetString(3);
-                        var channelId = reader.GetString(4);
-                        var locale = reader.GetString(5);
-                        var serviceUrl = reader.GetString(6);
+                        var appId = reader.GetString(0);
 
-                        log.LogInformation($"DB record id:{id} botId:{botId} userId:{userId} userName:{userName} channelId:{channelId} local:{locale} serviceUrl:{serviceUrl}");
-                        string record = $"{id}|{botId}|{userId}|{userName}|{channelId}|{locale}|{serviceUrl}";
+                        log.LogInformation($"Getting unique appId {appId} start creating...");
 
-                        if(!messageBatch.TryAddMessage(new ServiceBusMessage(record)))
+                        var managementClient = new ManagementClient(Environment.GetEnvironmentVariable("SBConnectString"));
+
+                        var allQueues = await managementClient.GetQueuesAsync();
+
+                        var foundQueue = allQueues.Where(q => q.Path == appId.ToLower()).SingleOrDefault();
+
+                        if (foundQueue == null)
                         {
-                            throw new Exception("To large for a message");
+                            await managementClient.CreateQueueAsync(appId);//add queue desciption properties
+
+                            log.LogInformation($"Queue {appId} is created");
                         }
+                        else
+                        {
+                            log.LogInformation($"Skip creating queue {appId} which already exists");
+                        }
+
+                        appList.Add(appId);
                     }
                 }
             }
 
-            try
+            foreach(string appId in appList)
             {
-                //await sender.SendMessagesAsync(messageBatch);
-                log.LogInformation("Send a new message");
-            }
-            finally
-            {
-                await sender.DisposeAsync();
-                await client.DisposeAsync();
+                var queueClient = new QueueClient(Environment.GetEnvironmentVariable("SBConnectString"), appId);
+
+                log.LogInformation($"Start sending message to queue {appId}");
+
+                string sql = @"select Id, TimeOffset, WorkHour from UserInfo where appId = '" + appId + "'";
+
+                using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var id = reader.GetString(0);
+                            var timeOffset = reader.GetInt32(1);
+                            var workHour = reader.GetInt32(2);
+
+                            log.LogInformation($"DB record id:{id} timeOffset:{timeOffset} workHour:{workHour}");
+                            string record = $"{id}";
+
+                            var message = new Message(Encoding.UTF8.GetBytes(record));
+                            await queueClient.SendAsync(message);
+
+                            log.LogInformation($"New message sent {record}");
+                        }
+                    }
+                }
+
+                await queueClient.CloseAsync();
             }
         }
     }
