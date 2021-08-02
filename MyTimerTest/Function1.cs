9ce837466f604c8dfdbb98e35132795df025efdc
Reggie Qin <reggieqin@microsoft.com>
2021-08-02 10:40:53 +0800

    update

--- MyTimerTest/Function1.cs
+++ MyTimerTest/Function1.cs
@@ -21,129 +21,92 @@ namespace MyTimerTest
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async Task Run([TimerTrigger("0 * * * * *")] TimerInfo myTimer,
-            [ServiceBus("meetingbrief", Connection = "SBConnectString", 
-            EntityType = Microsoft.Azure.WebJobs.ServiceBus.EntityType.Topic)] IAsyncCollector<ServiceBusMessage> output, 
-            ILogger log)
+        public static async Task Run([TimerTrigger("0 */30 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"Reggie Timer trigger function executed at: {DateTime.Now}");
 
-            try
+            DataTable userData = await getUserDataFromDB(log);
+
+            await SendUserDataToMQ(userData, log);
+        }
+
+        public static async Task<DataTable> getUserDataFromDB(ILogger log)
+        {
+            SecretClient secretClient = new SecretClient(new Uri(Environment.GetEnvironmentVariable("KeyVaultEndpoint")), new DefaultAzureCredential());
+            KeyVaultSecret userId = await secretClient.GetSecretAsync($"SQL-userId");
+            KeyVaultSecret password = await secretClient.GetSecretAsync($"SQL-password");
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = Environment.GetEnvironmentVariable("DataSource");
+            builder.UserID = userId.Value;
+            builder.Password = password.Value;
+            builder.InitialCatalog = Environment.GetEnvironmentVariable("SchedulerDB");
+
+            SqlConnection sqlConnection = new SqlConnection(builder.ConnectionString);
+            sqlConnection.Open();
+
+            DataTable dataTable = new DataTable();
+
+            string userDataSql = $"EXEC GetUserData";
+
+            log.LogInformation($"Start executing sql {userDataSql}");
+
+            using (SqlCommand command = new SqlCommand(userDataSql, sqlConnection))
             {
-                var message = new ServiceBusMessage(Encoding.UTF8.GetBytes("test"));
-                message.ApplicationProperties.Add("id", "123");
-                message.ApplicationProperties.Add("appId", "1234");
-                message.ApplicationProperties.Add("goal", 10);
-                message.CorrelationId = "1234";
-                message.SessionId = "1234";
-                message.Subject = "1234";
-                message.TimeToLive = new TimeSpan(2, 0, 0, 0);
-
-                await output.AddAsync(message);
-
-                var message2 = new ServiceBusMessage(Encoding.UTF8.GetBytes("test2"));
-                message2.ApplicationProperties.Add("id", "123");
-                message2.ApplicationProperties.Add("appId", "5678");
-                message2.ApplicationProperties.Add("goal", 5);
-                message2.CorrelationId = "5678";
-                message2.SessionId = "5678";
-                message2.Subject = "5678"; 
-                message2.TimeToLive = new TimeSpan(2, 0, 0, 0);
-
-                await output.(message2);
+                using (SqlDataAdapter da = new SqlDataAdapter(command))
+                {
+                    try
+                    {
+                        da.Fill(dataTable);
+                        log.LogInformation("Retrieving data from SQL successfully");
+                    }
+                    catch(Exception e)
+                    {
+                        log.LogError($"Retrieving data from SQL failed due to {e.Message}");
+                    }
+                    finally
+                    {
+                        da.Dispose();
+                    }
+                }
             }
-            catch(Exception e)
+
+            sqlConnection.Close();
+
+            return dataTable;
+        }
+
+        public static async Task SendUserDataToMQ(DataTable userData, ILogger log)
+        {
+            List<ServiceBusMessage> serviceBusMessages = new List<ServiceBusMessage>();
+            foreach (DataRow row in userData.Rows)
             {
-                log.LogError("Error " + e.Message);
+                string id = row[userData.Columns[0]] as string;
+                string appId = row[userData.Columns[1]] as string;
+
+                var serviceBugMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes("meetingbrief"));
+                serviceBugMessage.ApplicationProperties.Add("id", id);
+                serviceBugMessage.ApplicationProperties.Add("appId", appId);
+                serviceBugMessage.TimeToLive = new TimeSpan(2, 0, 0, 0);
+
+                serviceBusMessages.Add(serviceBugMessage);
+
+                log.LogInformation($"Adding message {id} {appId}");
             }
 
+            if (serviceBusMessages.Count > 0)
+            {
+                var _client = new ServiceBusClient(Environment.GetEnvironmentVariable("SBConnectString"));
+                var _clientSender = _client.CreateSender(Environment.GetEnvironmentVariable("TopicName"));
 
-            //SecretClient secretClient = new SecretClient(new Uri(Environment.GetEnvironmentVariable("KeyVaultEndpoint")), new DefaultAzureCredential());
-            //KeyVaultSecret userId = await secretClient.GetSecretAsync($"SQL-userId");
-            //KeyVaultSecret password = await secretClient.GetSecretAsync($"SQL-password");
+                await _clientSender.SendMessagesAsync(serviceBusMessages).ConfigureAwait(false);
 
-            //SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            //builder.DataSource = Environment.GetEnvironmentVariable("DataSource");
-            //builder.UserID = userId.Value;
-            //builder.Password = password.Value;
-            //builder.InitialCatalog = Environment.GetEnvironmentVariable("SchedulerDB");
-
-            //log.LogInformation($"DB configuration source:{builder.DataSource} userId:{builder.UserID} pwd:{builder.Password} db:{builder.InitialCatalog}");
-
-            //DateTime currentUTCDatetime = DateTime.Now.ToUniversalTime();
-            //string currentUTCTime = currentUTCDatetime.ToString("HH:00:00");
-
-            //SqlConnection sqlConnection = new SqlConnection(builder.ConnectionString);
-            //sqlConnection.Open();
-
-            //// Get unique appId
-            //IDictionary<string, IList<string>> appUserDataDict = new Dictionary<string, IList<string>>();
-
-            //DataTable dataTable = new DataTable();
-
-            //string userDataSql = $"EXEC GetUserData";
-
-            //using (SqlCommand command = new SqlCommand(userDataSql, sqlConnection))
-            //{
-            //    using (SqlDataAdapter da = new SqlDataAdapter(command))
-            //    {
-            //        try
-            //        {
-            //            da.Fill(dataTable);
-
-            //            foreach (DataRow row in dataTable.Rows)
-            //            {
-            //                string appId = row[dataTable.Columns[1]] as string;
-            //                if (!appUserDataDict.ContainsKey(appId))
-            //                {
-            //                    appUserDataDict[appId] = new List<string>();
-            //                }
-            //                appUserDataDict[appId].Add(row[dataTable.Columns[0]] as string);
-            //            }
-            //        }
-            //        finally
-            //        {
-            //            da.Dispose();
-            //        }
-
-            //    }
-            //}
-
-            //sqlConnection.Close();
-
-            //foreach (var app in appUserDataDict)
-            //{
-            //    log.LogInformation($"Getting unique appId {app.Key} start creating...");
-
-            //    var managementClient = new ManagementClient(Environment.GetEnvironmentVariable("SBConnectString"));
-
-            //    var allQueues = await managementClient.GetQueuesAsync();
-
-            //    var foundQueue = allQueues.Where(q => q.Path == app.Key.ToLower()).SingleOrDefault();
-
-            //    if (foundQueue == null)
-            //    {
-            //        await managementClient.CreateQueueAsync(app.Key);//add queue desciption properties
-
-            //        log.LogInformation($"Queue {app.Key} is created");
-            //    }
-            //    else
-            //    {
-            //        log.LogInformation($"Skip creating queue {app.Key} which already exists");
-            //    }
-
-            //    var queueClient = new QueueClient(Environment.GetEnvironmentVariable("SBConnectString"), app.Key);
-
-            //    foreach (var userData in app.Value)
-            //    {
-            //        string record = $"{userData}";
-
-            //        var message = new Message(Encoding.UTF8.GetBytes(record));
-            //        await queueClient.SendAsync(message);
-
-            //        log.LogInformation($"New message sent {record} to {app.Key}");
-            //    }
-            //}
+                log.LogInformation($"Total {serviceBusMessages.Count} messages sent");
+            }
+            else
+            {
+                log.LogInformation("No message to send");
+            }
         }
     }
 }
